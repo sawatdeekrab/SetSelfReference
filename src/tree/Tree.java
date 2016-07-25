a4366013038687237e6b30c1f6ee6d37e32b89bb
User <User@DESKTOP-PHUECK5>
2016-07-25 20:34:27 +0700

    Remove Unused Dependency

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tree;
 
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import javax.swing.text.html.HTMLDocument;
 
-/**
- *
- * @author User
- */
+
+
 public class Tree {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Branch root = new Branch();
 
